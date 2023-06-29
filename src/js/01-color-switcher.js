954ddd796238fb41fea0b3acc362bae23c718f67
Yuliia Chaika <djuliya72@ukr.net>
2023-06-29 17:08:43 +0300

    finished task1

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,29 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+
+btnStart.addEventListener('click', onBtnStart);
+btnStop.addEventListener('click', onBtnStop);
+let timerId = null;
+
+function onBtnStart() {
+    timerId = setInterval(changeBodyColor, 1000);
+    btnStart.disabled = true;
+    btnStop.disabled = false;
+    console.log(timerId);
+}
+
+function changeBodyColor() {
+    document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+
+
+function onBtnStop() {
+    clearInterval(timerId);
+    btnStop.disabled = true;
+    btnStart.disabled = false;
+    console.log(timerId);
+}
